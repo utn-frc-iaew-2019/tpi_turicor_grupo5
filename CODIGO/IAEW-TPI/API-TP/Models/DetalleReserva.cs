e579aac02e4331cae84d59b081eb2c1b06079749
MisaelPecorari <42780892+MisaelPecorari@users.noreply.github.com>
2019-06-28 14:44:11 -0300

    Desarrollo API

---
+++ CODIGO/IAEW-TPI/API-TP/Models/DetalleReserva.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API_TP.Models
+{
+    public class DetalleReserva
+    {
+        public string ApellidoNombreCliente { get; set; }
+        public DateTime FechaHoraDevolucion { get; set; }
+        public DateTime FechaHoraRetiro { get; set; }
+        public int IDVehiculoCiudad { get; set; }
+        public string LugarRetiro { get; set; }
+        public string LugarDevolucion { get; set; }
+        public string NroDocumentoCliente { get; set; }
+    }
+}
